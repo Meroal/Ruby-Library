e6f1d025e13fc1d0404cea3ec7b5e1e04023aa67
Ash Angell <ash@ashleyangell.com>
2019-06-12 13:24:57 +1000

    #22 Added API method to fetch a credit card by token.

--- lib/fat_zebra/card.rb
+++ lib/fat_zebra/card.rb
@@ -4,19 +4,20 @@ module FatZebra
   #
   # Manage credit card for the API
   #
+  # * find
   # * save
   #
   class Card < APIResource
     @resource_name = 'credit_cards'
 
     include FatZebra::APIOperation::Save
+    include FatZebra::APIOperation::Find
 
     validates :card_holder, required: { unless: %i[wallet] }, on: :create
     validates :card_number, required: { unless: %i[wallet] }, on: :create
     validates :card_expiry, required: { unless: %i[wallet] }, on: :create
 
     validates :wallet, required: { unless: %i[card_number] }, on: :create
-
     validates :card_expiry, required: true, on: :update
 
   end
--- spec/lib/fat_zebra/card_spec.rb
+++ spec/lib/fat_zebra/card_spec.rb
@@ -2,20 +2,35 @@ require 'spec_helper'
 
 describe FatZebra::Card do
 
-  describe '.create', :vcr do
-    subject(:credit_card) { FatZebra::Card.create(valid_credit_card_payload) }
+  describe '.find', :vcr do
+    let!(:credit_card) {FatZebra::Card.create(valid_credit_card_payload)}
+    subject(:card) {FatZebra::Card.find(credit_card.token)}
 
-    it { is_expected.to be_accepted }
-    it { expect(credit_card.token).to_not be_empty }
-    it { expect(credit_card.card_holder).to eq('Matthew Savage') }
-    it { expect(credit_card.card_number).to_not be_empty }
-    it { expect(credit_card.card_type).to eq('MasterCard') }
-    it { expect(credit_card.authorized?).to be_truthy }
+    it {is_expected.to be_accepted}
+    it {expect(card.card_number).to eq(credit_card.card_number)}
+    it {expect(card.card_expiry).to eq(credit_card.card_expiry)}
+
+    context 'invalid card token' do
+      subject(:card) {FatZebra::Card.find('invalid-token')}
+      it {expect {card}.to raise_error(FatZebra::RequestError)}
+    end
+  end
+
+
+  describe '.create', :vcr do
+    subject(:credit_card) {FatZebra::Card.create(valid_credit_card_payload)}
+
+    it {is_expected.to be_accepted}
+    it {expect(credit_card.token).to_not be_empty}
+    it {expect(credit_card.card_holder).to eq('Matthew Savage')}
+    it {expect(credit_card.card_number).to_not be_empty}
+    it {expect(credit_card.card_type).to eq('MasterCard')}
+    it {expect(credit_card.authorized?).to be_truthy}
 
     context 'validations' do
       let(:valid_credit_card_payload) {{}}
 
-      it { expect{ credit_card }.to raise_error(FatZebra::RequestValidationError) }
+      it {expect {credit_card}.to raise_error(FatZebra::RequestValidationError)}
     end
   end
 
@@ -24,17 +39,17 @@ describe FatZebra::Card do
       card_expiry: DateTime.new(2050, 2, 3).strftime('%m/%Y'),
     }}
 
-    let(:create) { FatZebra::Card.create(valid_credit_card_payload) }
-    subject(:credit_card) { FatZebra::Card.update(create.token, valid_credit_card_update_payload) }
+    let(:create) {FatZebra::Card.create(valid_credit_card_payload)}
+    subject(:credit_card) {FatZebra::Card.update(create.token, valid_credit_card_update_payload)}
 
-    it { is_expected.to be_accepted }
-    it { expect(credit_card.token).to eq(create.token) }
-    it { expect(credit_card.card_expiry).to_not eq(create.card_expiry) }
+    it {is_expected.to be_accepted}
+    it {expect(credit_card.token).to eq(create.token)}
+    it {expect(credit_card.card_expiry).to_not eq(create.card_expiry)}
 
     context 'validations' do
       let(:valid_credit_card_update_payload) {{}}
 
-      it { expect{ credit_card }.to raise_error(FatZebra::RequestValidationError) }
+      it {expect {credit_card}.to raise_error(FatZebra::RequestValidationError)}
     end
   end
 
